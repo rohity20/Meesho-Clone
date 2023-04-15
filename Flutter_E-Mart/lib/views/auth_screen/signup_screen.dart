d2349ac0a7359cc71d86f0a811ffb4eac9b7ab69
Rohit Yadav <2020.rohit.yadav@ves.ac.in>
2023-04-15 15:40:38 +0530

    commited

---
+++ Flutter_E-Mart/lib/views/auth_screen/signup_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+import '../../consts/consts.dart';
+import '../../consts/lists.dart';
+import '../../widgets_common/applogo_widget.dart';
+import '../../widgets_common/bg_widget.dart';
+import '../../widgets_common/custom_textfield.dart';
+import '../../widgets_common/our_button.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return bgWidget(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Column(children: [
+          (context.screenHeight * 0.1).heightBox,
+          applogoWidget(),
+          10.heightBox,
+          "Join the $appname".text.fontFamily(bold).white.size(18).make(),
+          10.heightBox,
+          Column(
+            children: [
+              customTextField(hint: nameHint, title: name),
+              customTextField(hint: emailHint, title: email),
+              customTextField(hint: passwordHint, title: password),
+              customTextField(hint: passwordHint, title: retypePassword),
+              Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {}, child: forgetPass.text.make())),
+              5.heightBox,
+              ourButton(
+                      color: redColor,
+                      title: signup,
+                      textColor: whiteColor,
+                      onPress: () {})
+                  .box
+                  .width(context.screenWidth - 50)
+                  .make(),
+              10.heightBox,
+              //wrapping into gesture detector of velocity X
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: alreadyHaveAccount,
+                      style: TextStyle(fontFamily: bold, color: fontGrey),
+                    ),
+                    TextSpan(
+                      text: login,
+                      style: TextStyle(fontFamily: bold, color: redColor),
+                    )
+                  ],
+                ),
+              ).onTap(() {
+                Get.back();
+              })
+            ],
+          )
+              .box
+              .white
+              .rounded
+              .padding(const EdgeInsets.all(16))
+              .width(context.screenWidth - 70)
+              .make(),
+        ]),
+      ),
+    ));
+  }
+}
---
+++ Flutter_E-Mart/lib/widgets_common/applogo_widget.dart
@@ -0,0 +1,14 @@
+import 'package:emart_app/consts/colors.dart';
+
+import '../consts/consts.dart';
+
+Widget applogoWidget() {
+  //using velocity X here
+  return Image.asset(icAppLogo)
+      .box
+      .white
+      .size(77, 77)
+      .padding(const EdgeInsets.all(8))
+      .rounded
+      .make();
+}
